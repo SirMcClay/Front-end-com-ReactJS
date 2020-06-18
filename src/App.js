f99abe3d4444a44a9415fbf549a679e332fe708e
sirmcclay <mcclay.design@gmail.com>
2020-06-18 12:09:28 -0300

    Front-end React

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+
+import Header from './components/Header';
+
+function App() {
+  const [projects, setProjects] = useState(['Desenvolvimento de app', 'Front-end web']);
+
+  function handleAddProject() {
+    setProjects([...projects, `Novo projeto ${Date.now()}`]);
+  }
+
+  return (
+    <>
+      <Header title="Projects" />
+
+      <ul>
+          {projects.map(project => <li key={project}>{project}</li>)}
+      </ul>
+
+      <button type="button" onClick={handleAddProject}>Adicionar projeto</button>
+    </>
+  )
+}
+
+export default App;
